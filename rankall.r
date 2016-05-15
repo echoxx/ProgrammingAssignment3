4eb01d12d3a13d5a443787e70725e9e1dde7d7cb
Xsess <snyder.xander@mykolab.com>
2016-05-15 14:10:11 -0700

    Functioning output. But creates individual data frames, rather than fixing order of master 'split states'

--- rankall.r
+++ rankall.r
@@ -1,15 +1,8 @@
-#WILL NEED TO REMOVE "INPUT_STATE" for final answer
-rankall <- function( input_state, metric, num) {
+rankall <- function( metric, position) {
   source <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available", stringsAsFactors=FALSE)
   
   #Renames columns of interest (easier to work with)
   names(source)[c(2,7,11,17,23)] <- c("hospital", "state", "heart_attack", "heart_failure", "pneumonia")
-  
-  #Checks if valid state
-  if ( all( input_state %in% source$state) == FALSE)  {
-    print("invalid state")
-    stop()
-  } 
   
   #Checks if valid metric
   if ( metric %in% names(source)  == FALSE)  {
@@ -37,24 +30,27 @@ rankall <- function( input_state, metric, num) {
   #BELOW, WORK ONLY WITH "REDUX" NOT "SOURCE"
   splitstate <- split(redux, redux$state)  #returns a list of data frames
   
-  #Maybe just use a for loop
-  orderstateFN <- function(df) {
-    for (i in seq_along(splitstate)) {
-      orderOutcomeAlpha <- order( splitstate[[i]][,3] , splitstate[[i]][,1] )
-      orderedstates <- splitstate[[i]][orderOutcomeAlpha,]
-      
-      if (i == 1) {
-        results <- orderedstates[1,]
-      } else {
-        results <- rbind(results,orderedstates[1,])
-      }
+  #Generates data frame with appropriate rank
+  for (i in seq_along(splitstate)) {
+    orderOutcomeAlpha <- order( splitstate[[i]][,3] , splitstate[[i]][,1] )
+    orderedstates <- splitstate[[i]][orderOutcomeAlpha,]
+    
+    #Sets "num" to rank, "best" or "worst"
+    num <- numeric()
+    if (position == "best") {
+      num <- 1
+    } else if (position == "worst") {
+      num <- length( splitstate[[i]][,3])
+    } else {
+      num <- position
     }
-    results
+    
+    #Collates hospital ranks by state  
+    if (i == 1) {
+      results <- orderedstates[num,]
+    } else {
+      results <- rbind(results,orderedstates[num,])
+    }
   }
-    #Function that outputs 1st hospital in each state --> need to change to ranking
-  
-  
-
-  
-  
+return(results) 
 }
